1414db19d2250ac1259c322f72f1af987d80faca
Ysj <543501451@qq.com>
2025-04-11 17:38:28 +0800

    feat：插件事件回调接口标识所在线程

--- lib_plugin/src/main/java/com/ysj/lib/android/stable/plugin/config/EventCallback.kt
+++ lib_plugin/src/main/java/com/ysj/lib/android/stable/plugin/config/EventCallback.kt
@@ -1,5 +1,6 @@
 package com.ysj.lib.android.stable.plugin.config
 
+import androidx.annotation.MainThread
 import com.ysj.lib.android.stable.plugin.Plugin
 
 /**
@@ -13,16 +14,19 @@ interface EventCallback {
     /**
      * 当初始化成功时回调。
      */
+    @MainThread
     fun onInitialized()
 
     /**
      * 当有插件被成功安装时回调。
      */
+    @MainThread
     fun onPluginInstalled(plugin: Plugin)
 
     /**
      * 当有插件被卸载时回调。
      */
+    @MainThread
     fun onPluginUninstalled(plugin: Plugin)
 
 }

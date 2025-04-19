e54d0fe8c1a83c6c4b350e4142e172afc4ddc5a3
Ysj <543501451@qq.com>
2025-04-19 22:32:56 +0800

    fix：修复直接从插件 application 获取 LayoutInflater 时无法加载插件布局的问题

--- lib_plugin/src/main/java/com/ysj/lib/android/stable/plugin/component/PluginApplication.kt
+++ lib_plugin/src/main/java/com/ysj/lib/android/stable/plugin/component/PluginApplication.kt
@@ -10,6 +10,7 @@ import android.content.res.loader.ResourcesLoader
 import android.content.res.loader.ResourcesProvider
 import android.os.Build
 import android.os.ParcelFileDescriptor
+import android.view.LayoutInflater
 import com.ysj.lib.android.stable.plugin.Plugin
 import com.ysj.lib.android.stable.plugin.StablePlugin
 import com.ysj.lib.android.stable.plugin.StablePlugin.pluginInstalledFile
@@ -27,6 +28,7 @@ internal open class PluginApplication : Application() {
     private lateinit var resources: Resources
 
     private var theme: Resources.Theme? = null
+    private var layoutInflater: LayoutInflater? = null
 
     public override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
@@ -87,6 +89,21 @@ internal open class PluginApplication : Application() {
 
     override fun getResources(): Resources {
         return resources
+    }
+
+    override fun getSystemService(name: String): Any? {
+        if (LAYOUT_INFLATER_SERVICE == name) {
+            var layoutInflater = this.layoutInflater
+            if (layoutInflater == null) {
+                layoutInflater = super.getSystemService(name) as LayoutInflater
+                layoutInflater = layoutInflater.cloneInContext(this)
+                layoutInflater.setFactory(PluginViewFactoryCompat(classLoader))
+                layoutInflater = layoutInflater.cloneInContext(this)
+                this.layoutInflater = layoutInflater
+            }
+            return layoutInflater
+        }
+        return super.getSystemService(name)
     }
 
     override fun getApplicationContext(): Context {

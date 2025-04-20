01a53e363f170c8ab8a5c0333b3e341cece72dcc
Ysj <543501451@qq.com>
2025-04-20 18:06:40 +0800

    feat：对 PluginProvider 的边界场景进行完善

--- lib_plugin/src/main/java/com/ysj/lib/android/stable/plugin/component/provider/PluginProvider.kt
+++ lib_plugin/src/main/java/com/ysj/lib/android/stable/plugin/component/provider/PluginProvider.kt
@@ -30,9 +30,7 @@ internal class PluginProvider : ContentProvider() {
         const val QUERY_KEY_PLUGIN_NAME = "plugin_name"
     }
 
-    private val Uri.pluginName get() = requireNotNull(getQueryParameter(QUERY_KEY_PLUGIN_NAME))
-    private val Uri.plugin get() = StablePlugin.findPluginByName(pluginName)
-    private val Uri.unwrap get() = requireNotNull(getQueryParameter(QUERY_KEY_WRAP)).toUri()
+    private val Uri.unwrap get() = getQueryParameter(QUERY_KEY_WRAP)?.toUri()
 
     override fun onCreate(): Boolean {
         Log.i(TAG, "onCreate.")
@@ -41,6 +39,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
         val unwrap = uri.unwrap
+            ?: return null
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.query(unwrap, projection, selection, selectionArgs, sortOrder)
             is ContentResolver -> contentInterface.query(unwrap, projection, selection, selectionArgs, sortOrder)
@@ -50,6 +49,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?, cancellationSignal: CancellationSignal?): Cursor? {
         val unwrap = uri.unwrap
+            ?: return super.query(uri, projection, selection, selectionArgs, sortOrder, cancellationSignal)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.query(unwrap, projection, selection, selectionArgs, sortOrder, cancellationSignal)
             is ContentResolver -> contentInterface.query(unwrap, projection, selection, selectionArgs, sortOrder, cancellationSignal)
@@ -60,6 +60,7 @@ internal class PluginProvider : ContentProvider() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun query(uri: Uri, projection: Array<out String>?, queryArgs: Bundle?, cancellationSignal: CancellationSignal?): Cursor? {
         val unwrap = uri.unwrap
+            ?: return super.query(uri, projection, queryArgs, cancellationSignal)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.query(unwrap, projection, queryArgs, cancellationSignal)
             is ContentResolver -> contentInterface.query(unwrap, projection, queryArgs, cancellationSignal)
@@ -69,6 +70,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun getType(uri: Uri): String? {
         val unwrap = uri.unwrap
+            ?: return null
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.getType(unwrap)
             is ContentResolver -> contentInterface.getType(unwrap)
@@ -79,6 +81,7 @@ internal class PluginProvider : ContentProvider() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun insert(uri: Uri, values: ContentValues?, extras: Bundle?): Uri? {
         val unwrap = uri.unwrap
+            ?: return super.insert(uri, values, extras)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.insert(unwrap, values, extras)
             is ContentResolver -> contentInterface.insert(unwrap, values, extras)
@@ -88,6 +91,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
         val unwrap = uri.unwrap
+            ?: return null
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.insert(unwrap, values)
             is ContentResolver -> contentInterface.insert(unwrap, values)
@@ -97,6 +101,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun bulkInsert(uri: Uri, values: Array<out ContentValues>): Int {
         val unwrap = uri.unwrap
+            ?: return super.bulkInsert(uri, values)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.bulkInsert(unwrap, values)
             is ContentResolver -> contentInterface.bulkInsert(unwrap, values)
@@ -107,6 +112,7 @@ internal class PluginProvider : ContentProvider() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun delete(uri: Uri, extras: Bundle?): Int {
         val unwrap = uri.unwrap
+            ?: return super.delete(uri, extras)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.delete(unwrap, extras)
             is ContentResolver -> contentInterface.delete(unwrap, extras)
@@ -116,6 +122,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
         val unwrap = uri.unwrap
+            ?: return 0
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.delete(unwrap, selection, selectionArgs)
             is ContentResolver -> contentInterface.delete(unwrap, selection, selectionArgs)
@@ -126,6 +133,7 @@ internal class PluginProvider : ContentProvider() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun update(uri: Uri, values: ContentValues?, extras: Bundle?): Int {
         val unwrap = uri.unwrap
+            ?: return super.update(uri, values, extras)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.update(unwrap, values, extras)
             is ContentResolver -> contentInterface.update(unwrap, values, extras)
@@ -135,6 +143,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
         val unwrap = uri.unwrap
+            ?: return 0
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.update(unwrap, values, selection, selectionArgs)
             is ContentResolver -> contentInterface.update(unwrap, values, selection, selectionArgs)
@@ -144,6 +153,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun openFile(uri: Uri, mode: String): ParcelFileDescriptor? {
         val unwrap = uri.unwrap
+            ?: return openFile(uri, mode)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.openFile(unwrap, mode)
             is ContentResolver -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -157,6 +167,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun openFile(uri: Uri, mode: String, signal: CancellationSignal?): ParcelFileDescriptor? {
         val unwrap = uri.unwrap
+            ?: return super.openFile(uri, mode, signal)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.openFile(unwrap, mode, signal)
             is ContentResolver -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -170,6 +181,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun openAssetFile(uri: Uri, mode: String): AssetFileDescriptor? {
         val unwrap = uri.unwrap
+            ?: return super.openAssetFile(uri, mode)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.openAssetFile(unwrap, mode)
             is ContentResolver -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -183,6 +195,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun openAssetFile(uri: Uri, mode: String, signal: CancellationSignal?): AssetFileDescriptor? {
         val unwrap = uri.unwrap
+            ?: return super.openAssetFile(uri, mode, signal)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.openAssetFile(unwrap, mode, signal)
             is ContentResolver -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -196,6 +209,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun getStreamTypes(uri: Uri, mimeTypeFilter: String): Array<String>? {
         val unwrap = uri.unwrap
+            ?: return super.getStreamTypes(uri, mimeTypeFilter)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.getStreamTypes(unwrap, mimeTypeFilter)
             is ContentResolver -> contentInterface.getStreamTypes(unwrap, mimeTypeFilter)
@@ -205,6 +219,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun openTypedAssetFile(uri: Uri, mimeTypeFilter: String, opts: Bundle?): AssetFileDescriptor? {
         val unwrap = uri.unwrap
+            ?: return super.openTypedAssetFile(uri, mimeTypeFilter, opts)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.openTypedAssetFile(unwrap, mimeTypeFilter, opts)
             is ContentResolver -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -218,6 +233,7 @@ internal class PluginProvider : ContentProvider() {
 
     override fun openTypedAssetFile(uri: Uri, mimeTypeFilter: String, opts: Bundle?, signal: CancellationSignal?): AssetFileDescriptor? {
         val unwrap = uri.unwrap
+            ?: return super.openTypedAssetFile(uri, mimeTypeFilter, opts, signal)
         return when (val contentInterface = findContentInterface(uri, unwrap)) {
             is ContentProvider -> contentInterface.openTypedAssetFile(unwrap, mimeTypeFilter, opts, signal)
             is ContentResolver -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -235,7 +251,8 @@ internal class PluginProvider : ContentProvider() {
         if (authority.isNullOrEmpty() || !authority.startsWith(context.packageName)) {
             return context.contentResolver
         }
-        val providerMap = uri.plugin
+        val providerMap = uri.getQueryParameter(QUERY_KEY_PLUGIN_NAME)
+            ?.let { StablePlugin.findPluginByName(it) }
             ?.providerMap
             ?: return context.contentResolver
         return providerMap[authority] ?: context.contentResolver
